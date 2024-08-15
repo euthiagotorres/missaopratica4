cf7dc07f1c9147c4904375dd200672fc006d973f
euthiagotorres <thiagot777@gmail.com>
2024-08-15 07:45:08 -0300

    Update PessoaFisica.py

--- PessoaFisica.py
+++ PessoaFisica.py
@@ -6,33 +6,33 @@ class PessoaFisica(Pessoa):
         self.__data_nascimento=data_nascimento
         self.__cpf=cpf
         self.__rg=rg
-        
-        @property
-        def data_nascimento(self):
-            return self.__data_nascimento
-        @data_nascimento.setter
-        def data_nascimento(self, value):
-            self.__data_nascimento=value
+
+    @property
+    def data_nascimento(self):
+        return self.__data_nascimento
+    @data_nascimento.setter
+    def data_nascimento(self, value):
+        self.__data_nascimento=value
+                
+    @property
+    def cpf(self):
+        return self.__cpf
+    @cpf.setter
+    def cpf(self, value):
+        if len(value)==11:
+            self.__cpf=value
+        else:
+            raise ValueError('CPF deve ter 11 caracteres.')
             
-        @property
-        def cpf(self):
-            return self.__cpf
-        @cpf.setter
-        def cpf(self, value):
-            if len(value)==11:
-                self.__cpf=value
-            else:
-                raise ValueError('CPF deve ter 11 caracteres.')
-            
-        @property
-        def rg(self):
-            return self.__rg
-        @rg.setter
-        def rg(self, value):
-            self.__rg=value
+    @property
+    def rg(self):
+        return self.__rg
+    @rg.setter
+    def rg(self, value):
+        self.__rg=value
         
-        def imprimir_dados(self):
-            super().imprimir_dados()
-            print(f'Data de nascimento:{self.data_nascimento}')
-            print(f'CPF:{self.cpf}')
-            print(f'RG:{self.rg}')
+    def imprimir_dados(self):
+        super().imprimir_dados()
+        print(f'Data de nascimento:{self.data_nascimento}')
+        print(f'CPF:{self.cpf}')
+        print(f'RG:{self.rg}')
